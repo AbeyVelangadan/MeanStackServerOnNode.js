adbdfb9058874b690a5f5acc38cd9aead0e9a361
Abey Velangadan <abey.velangadan@gmail.com>
2015-08-25 01:29:59 +0530

    MEAN STACK - Server
    Web Server using Node.js & Mongo DB. This doesn't include the set
    up/config info for Mongo

---
+++ NodejsConsoleApp1/ReadWrite.js
@@ -0,0 +1,48 @@
+﻿var mongooseDrv = require('mongoose'); //Get the Mongoose Driver
+//connection with theMongoDB
+
+
+mongooseDrv.connect('mongodb://localhost/EmployeeDB');
+var db = mongooseDrv.Connection; //The Connection 
+
+if (db == 'undefined') {
+	console.log("The Connecion issues");
+}
+
+//The Schema for the Data to be Stored
+var EmployeeInfoSchema = mongooseDrv.Schema({
+	EmpNo: String,
+	EmpName: String,
+	Salary: String,
+	DeptName: String,
+	Designation: String
+});
+
+var EmployeeInfoModel = mongooseDrv.model('EmployeeInfo', EmployeeInfoSchema);
+
+
+//retrieve all records from the database 
+exports.get = function (req, resp) {
+	EmployeeInfoModel.find().exec(function (error, res) {
+		if (error) {
+			resp.send(500, { error: error });
+		} else {
+			resp.send(res);
+		}
+ 
+	});
+};
+
+//Add a new Record in the Employee Model
+exports.add = function (request, response) {
+	
+	var newEmp = { EmpNo: request.body.EmpNo, EmpName: request.body.EmpName, Salary: request.body.Salary, DeptName: request.body.DeptName, Designation: request.body.Designation };
+	EmployeeInfoModel.create(newEmp, function (addError, addedEmp) {
+		if (addError) {
+			response.send(500, { error: addError });
+		}
+		else {
+			response.send({ success: true, emp: addedEmp });
+		}
+	});
+};
---
+++ NodejsConsoleApp1/app.js
@@ -0,0 +1,23 @@
+﻿var body_Parser = require('body-parser'); //Load Modules for bodyparser
+var path = require('path'); //Load 'path' the File base module 
+var express = require('express'); //Load express module
+var http = require('http'); //Load Http module for Http operation
+
+var app = express(); //The Express object
+
+// app.use(body_Parser());
+app.use(body_Parser.urlencoded({ extended: true }));
+
+var rwOperation = require('./ReadWrite.js'); //Load the File
+
+var communicationPort = 8080; //The Communication port
+
+//The REST API
+
+app.get('/EmployeeList/api/employees', rwOperation.get);
+
+app.post('/EmployeeList/api/employees', rwOperation.add);
+
+
+app.listen(communicationPort);
+console.log('listening....');
